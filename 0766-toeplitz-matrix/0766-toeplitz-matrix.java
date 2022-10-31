ca6cd405946367480d3aef8b856bd608fb1174cf
NishanthAR-droid <85539246+NishanthAR-droid@users.noreply.github.com>
2022-10-31 19:49:48 +0530

    Time: 2 ms (42.33%), Space: 46.2 MB (21.85%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        for(int i=1;i<matrix.length;i++)
+        for(int i=0;i<matrix.length-1;i++)
         {
-            for(int j=1;j<matrix[i].length;j++)
-                if(matrix[i][j]!=matrix[i-1][j-1])
+            for(int j=0;j<matrix[i].length-1;j++)
+                if(matrix[i][j]!=matrix[i+1][j+1])
                     return false;
         }
         return true;
