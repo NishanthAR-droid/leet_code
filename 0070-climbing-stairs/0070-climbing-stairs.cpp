854735d82b2188534024dc5b59cd7aed1ee05eed
NishanthAR-droid <85539246+NishanthAR-droid@users.noreply.github.com>
2023-08-19 13:30:01 +0530

    Time: 0 ms (100.00%), Space: 6 MB (58.78%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n == 1) {
-            return 1;
+        if(n<=3) return n;
+        
+        int first =1,second=1;
+        
+        int result=0;
+        
+        for(int i=0;i<n-1;i++)
+        {
+            result=first+second;
+            second=first;
+            first= result;
         }
-        if (n == 2) {
-            return 2;
-        }
-        
-        int first = 1;
-        int second = 2;
-        
-        int result = 0;
-        
-        for (int i = 2; i < n; i++) {
-            result = first + second;
-            first = second;
-            second = result;
-        }
-        
         return result;
     }
 };
