cf2738cc518446aaad8578942f41515735d1c8cc
NishanthAR-droid <85539246+NishanthAR-droid@users.noreply.github.com>
2024-03-19 11:31:25 +0530

    Time: 2 ms (92.84%), Space: 46.6 MB (36.08%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,20 +1,40 @@
 class Solution {
     public int[] countBits(int n) {
         //brute force time complexity:O(n log n)
-        int []ans=new int[n+1];
+//         int []ans=new int[n+1];
     
-        for(int i=0;i<=n;i++)
-        {
-            ans[i]=0;
-            int x=i;
-            while(x!=0)
-            {
-                if(x%2==1)
-                    ans[i]++;
-                x=x/2;
-            }
-        }
-        return ans;
+//         for(int i=0;i<=n;i++)     //for loop is O(n)
+//         {
+//             ans[i]=0;
+//             int x=i;
+//             while(x!=0)           //while loop is O(log n)
+//             {
+//                 if(x%2==1)
+//                     ans[i]++;
+//                 x=x/2;
+//             }
+//         }
+//         return ans;
         
+        //optimized code
+        
+        int ans[]=new int[n+1];
+        ans[0]=0;
+        if(n==0)
+            return ans;
+        
+        ans[1]=1;
+        if(n==1)
+            return ans;
+        
+        for(int i=2;i<=n;i++)
+        {
+            if(i%2==0)
+                ans[i]=ans[i/2];
+            else
+                ans[i]=ans[i/2]+1;
+        }
+        
+        return ans;
     }
 }
