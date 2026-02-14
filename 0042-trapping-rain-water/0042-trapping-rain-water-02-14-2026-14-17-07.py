6346f11315384ba6cd79943ee45e6066fabeb780
NishanthAR-droid <85539246+NishanthAR-droid@users.noreply.github.com>
2026-02-14 14:17:07 +0530

    Time: 17 ms (28.14%), Space: 21 MB (55.04%), Difficultly:  Hard

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water-02-14-2026-14-17-07.py
@@ -0,0 +1,19 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        leftmax = [0]*n
+        rightmax = [0]*n
+        water = 0
+
+        for i in range(1,n):
+            leftmax[i] = max(leftmax[i-1], height[i-1])
+
+        for i in range(n-2,0,-1):
+            rightmax[i] = max(rightmax[i+1], height[i+1])
+
+        for i in range(1,n-1):
+            if min(leftmax[i],rightmax[i])>height[i]:
+                water += min(leftmax[i],rightmax[i])-height[i]
+
+        return water        
+
